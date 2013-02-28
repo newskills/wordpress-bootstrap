023bed8099adbf768cd4713913c5b3940168e3c4
Mads Nordholm <mads@nordholm.dk>
2013-02-28 14:44:17 +0100

    Added the right fonts and fiddled with typography

--- index.php
+++ index.php
@@ -17,18 +17,20 @@
 			
 			<div id="content" class="clearfix row-fluid home-content">
 			
-				<div class="span4 home-left">
+				<div class="span4 home-one">
 					<img src="<?php bloginfo('template_directory'); ?>/images/ns_indgang.jpg" alt="NEW Skills indgangsparti">
 					<h2>Åbent hus</h2>
 					<p class="date">Onsdag 6 . marts kl. 9.30</p>
 					<p>HVER ANDEN ONSDAG ÅBNER VI DØRENE FOR NUVÆRENDE OG TIDL. KURSISTER, DER HAR BRUG FOR VEJLEDNING
 						MED AKTUELLE OPGAVER. KOM SENEST KL. 18.30</p>
 				</div>
-				<div class="span8 home-right">
-					<a href="/kalenderkategorier/kurser">
-						<h2>SAMMENSÆT DIT<br>KURSUS HER</h2>
-						<p>Eller kontakt os for personlig vejledning på Tlf. 70 21 04 00</p>
-					</a>
+				<div class="span4 home-two">
+					<h2>Testimonials</h2>
+					<p>Udtalelser fra kursister</p>
+				</div>
+				<div class="span4 home-three">
+					<h2>Kalender</h2>
+					<p>Kalender widget</p>
 				</div>
     
 			</div> <!-- end #content -->
