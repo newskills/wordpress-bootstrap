59f80236cb2a8f3783981d4439dfad6106d7c6ec
Mads Nordholm <mads@nordholm.dk>
2013-03-18 11:39:51 +0100

    A few minor changes to test if the blog branch works as intended

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			
 			<div id="content" class="clearfix row-fluid home-content">
 			
-				<div class="span4 home-one">
+				<div class="span12">
 					<div class="clearfix">
 						<h2><span class="light">IT-</span>Afklaring</h2>
 						<p>Har du brug for kurser? I løbet af en uge kan du få afklaret dine faglige kompetencer og lavet et
@@ -25,32 +25,6 @@
 							<a href="/a/it-afklaring/" class="pull-right clearfix">Læs mere om IT-afklaring</a>
 					</div>
 
-					<h2><span class="light">Laaang</span> onsdag</h2>
-					<p class="date">onsdag 20. marts kl. 9</p>
-					<p>Hver anden onsdag åbner vi dørene for nuværende og tidligere kursister, der har brug for vejledning
-						med aktuelle opgaver. Kom senest kl. 18.30</p>
-				</div>
-				<div class="span4 home-two">
-					<h2>Testimonials</h2>
-					<p class="testimonial"><em>”Super godt sted og rigtig god undervisning. Det her giver mig helt klart
-						bedre muligheder for at få et job”</em>
-						<span>- Peter, <a href="/a/medielab">MedieLAB</a></span>
-					</p>
-
-					<p class="testimonial"><em>”Tak for to go'e og inspirerende måneder hos jer - jeg er godt rustet til at
-						søge flere forskellige typer jobs nu - indenfor webkommunikation.”</em>
-						<span>- Ina, <a href="/a/webkommunikation-cms">Webkommunikation og CMS</a></span>
-					</p>
-
-					<p class="testimonial"><em>”Det er ligesom at komme på arbejde og møde ens søde kollegaer, man får input
-						og her er altid nogen at sparre med”</em>
-						<span>-	Anne, <a href="/a/online-marketing-og-sociale-medier">Online Marketing og Sociale Medier</a></span>
-					</p>
-				</div>
-				<div class="span4 home-three">
-					<?php echo do_shortcode( '[eo_calendar]' ); ?>
-				</div>
-    
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
